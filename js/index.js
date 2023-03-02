59b2d3bf8a3fbc6023338e211eebc0a810ed0513
maruf2446 <maruf2446@gmail.com>
2023-03-02 10:21:19 -0800

    add comit

--- js/index.js
+++ js/index.js
@@ -1,25 +1,45 @@
 const lodUser = () =>{
     fetch('https://openapi.programming-hero.com/api/ai/tools')
     .then(res => res.json())
-    .then(data => displayUniverse(data.data.tools))
+    .then(data => displayUniverse(data.data.tools,))
 }
 
 const displayUniverse = universes =>{
     // console.log(universe);
     const universeContainer = document.getElementById('universe-container');
+    // display 6 universes card
+    const showAll = document.getElementById('show-all');
+    if(universes.length > 6){
+        universes = universes.slice(0, 6);
+        showAll.classList.remove('d-none')
+    }
+    else{
+        showAll.classList.add('d-none');
+    }
+
+    // document.getElementById('btn-show-all').addEventListener('click', function(){
+    //     universes();
+    // })
+
     universes.forEach(universe => {
         console.log(universe);
         const universeDiv = document.createElement('div');
         universeDiv.classList.add('col');
         universeDiv.innerHTML = `
         <div class="card h-100">
-          <img src="..." class="card-img-top" alt="...">
+          <img src="${universe.image}" class="card-img-top" alt="...">
             <div class="card-body">
-               <h5 class="card-title">Card title</h5>
-               <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+               <h5 class="">Features</h5>
+               <p class="mt-4">${universe.features[0]}</p>
+               <p class="">${universe.features[1]}</p>
+               <p class="">${universe.features[2]}</p>
+            </div>
+            <div class="card-footer">
+              <p class="">${universe.name}</p>
+              <p class="">${universe.published_in}</p>
             </div>
         </div>
-        `
+        `;
         universeContainer.appendChild(universeDiv);
     });
 }
