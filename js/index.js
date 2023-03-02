3512eee41cebff444612019363dcc700904ba92e
maruf2446 <maruf2446@gmail.com>
2023-03-02 12:34:06 -0800

    added commit

--- js/index.js
+++ js/index.js
@@ -1,28 +1,21 @@
+const universeContainer = document.getElementById('universe-container');
+const AllData = []
 const lodUser = () =>{
     fetch('https://openapi.programming-hero.com/api/ai/tools')
     .then(res => res.json())
-    .then(data => displayUniverse(data.data.tools,))
+    .then(data => {
+        displayUniverse(data.data.tools);
+        AllData.push(data.data.tools);
+    })
 }
 
-const displayUniverse = universes =>{
+const displayUniverse = datas =>{
     // console.log(universe);
-    const universeContainer = document.getElementById('universe-container');
-    // display 6 universes card
-    const showAll = document.getElementById('show-all');
-    if(universes.length > 6){
-        universes = universes.slice(0, 6);
-        showAll.classList.remove('d-none')
-    }
-    else{
-        showAll.classList.add('d-none');
-    }
-
-    // document.getElementById('btn-show-all').addEventListener('click', function(){
-    //     universes();
-    // })
+       // display 6 universes card
+    let universes = datas?.slice(0, 6);
 
     universes.forEach(universe => {
-        console.log(universe);
+        // console.log(universe);
         const universeDiv = document.createElement('div');
         universeDiv.classList.add('col');
         universeDiv.innerHTML = `
@@ -34,9 +27,48 @@ const displayUniverse = universes =>{
                <p class="">${universe.features[1]}</p>
                <p class="">${universe.features[2]}</p>
             </div>
-            <div class="card-footer">
-              <p class="">${universe.name}</p>
-              <p class="">${universe.published_in}</p>
+            <div class="card-footer d-flex justify-content-between p-4">
+               <div>
+                 <p class="">${universe.name}</p>
+                 <p class="">${universe.published_in}</p>
+               </div>
+               <div>
+                 <button><i class="fa-solid fa-arrow-right"></i></button>
+               </div>
+            </div>
+        </div>
+        `;
+        universeContainer.appendChild(universeDiv);
+    });
+}
+
+document.getElementById('btn-show-all').addEventListener('click', () =>{
+    universeContainer.textContent = ''
+     showAllFun(AllData[0]);
+})
+
+const showAllFun = datas =>{
+    datas.forEach(universe => {
+        // console.log(universe);
+        const universeDiv = document.createElement('div');
+        universeDiv.classList.add('col');
+        universeDiv.innerHTML = `
+        <div class="card h-100">
+          <img src="${universe.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+               <h5 class="">Features</h5>
+               <p class="mt-4">${universe.features[0]}</p>
+               <p class="">${universe.features[1]}</p>
+               <p class="">${universe.features[2]}</p>
+            </div>
+            <div class="card-footer d-flex justify-content-between p-4">
+               <div>
+                 <p class="">${universe.name}</p>
+                 <p class="">${universe.published_in}</p>
+               </div>
+               <div>
+                 <button><i class="fa-solid fa-arrow-right"></i></button>
+               </div>
             </div>
         </div>
         `;
